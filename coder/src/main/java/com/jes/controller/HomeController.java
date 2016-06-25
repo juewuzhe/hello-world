d8f4c115d9e04d8f4ce38b82916620982e51e794
tingzhi.wang <tingzhi.wang@jereh.com>
2016-06-25 16:26:57 +0800

    spring mvc

---
+++ coder/src/main/java/com/jes/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.jes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("index")
+	public ModelAndView index(){
+		ModelAndView mav = new ModelAndView("home");
+		return mav;
+	}
+}
